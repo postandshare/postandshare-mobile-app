f1614cd3063f99f05009d8febda40579dcadd364
Alok Rawat <alokrawat2121@gmail.com>
2024-02-14 09:21:03 +0530

    working on bussiness profile

--- src/screens/mybussiness/AddBussiness.js
+++ src/screens/mybussiness/AddBussiness.js
@@ -4,16 +4,59 @@ import TopHeader from '../../components/TopHeader';
 import BussinessSelect from './components/BussinessSelect';
 import CustomButton from '../../components/CustomButton';
 import BussinessType from './components/BussinessType';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 const AddBussiness = () => {
   const [bussinessType, setBussinessType] = React.useState('Bussiness');
-
   const [orgBussinessType, setOrgBussinessType] = useState('');
+
+  const bussinessTypeFormik = useFormik({
+    initialValues: {
+      logo: '',
+      bussinessCategory: '',
+      bussinessSubCategory: '',
+
+      // bussinessName: '',
+      // bussinessDetail: '',
+      // bussinessEmail: '',
+      // bussinessWebsite: '',
+      // bussinessAddress: '',
+      // yourName: '',
+      // yourDesignation: '',
+      // yourMobile: '',
+      // yourEmail: '',
+      // yourwhatsapp: '',
+
+      // isbussinessPatner: '',
+      // bussinessPartnerName: '',
+      // bussinessPartnerDesignation: '',
+      // bussinessPartnerMobile: '',
+      // bussinessPartnerEmail: '',
+      // bussinessPartnerProfilePic: '',
+
+    },
+    validationSchema: yup.object({
+      logo: yup
+        .string()
+        .required('Transport using is required'),
+
+      bussinessCategory: yup.string().required('Bussiness Category is required'),
+      bussinessSubCategory: yup.string().required('Bussiness Sub Category is required'),
+    }),
+    onSubmit: formValues => {
+      console.log(formValues, 'in addbussiness formik');
+    },
+  });
+
+
+
 
   return (
     <>
       <TopHeader titile={'AddBussiness'} />
 
+      {/* bussiness selection */}
       {!orgBussinessType && (
         <>
           {/* selection for bussiness */}
@@ -34,10 +77,10 @@ const AddBussiness = () => {
         </>
       )}
 
-      
+
 
       {orgBussinessType == 'Bussiness' ? (
-        <BussinessType/>
+        <BussinessType bussinessTypeFormik={bussinessTypeFormik}/>
       ) : orgBussinessType == 'Political' ? (
         <View>
           <Text>This is political bussiness</Text>
--- src/screens/mybussiness/components/BussinessType.js
+++ src/screens/mybussiness/components/BussinessType.js
@@ -2,10 +2,12 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Colors from '../../../constants/Colors';
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
-import Ionicons from "react-native-vector-icons/Ionicons"
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import CustomButton from '../../../components/CustomButton';
 
-const BussinessType = () => {
+const BussinessType = ({bussinessTypeFormik}) => {
+  const [formStep, setFormStep] = React.useState(1);
   return (
     <>
       <View
@@ -18,33 +20,20 @@ const BussinessType = () => {
         {/* bussiness type */}
         <View style={{alignItems: 'center'}}>
           <View
-            style={{
-              backgroundColor: Colors.PRIMARY,
-              borderWidth: 1,
-              borderRadius: 10,
-              borderColor: Colors.TEXT1,
-              height: 40,
-              width: 40,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+            style={[
+              formStep === 1 ? {backgroundColor: Colors.PRIMARY} : null,
+              styles.container,
+            ]}>
             <Entypo name="user" size={20} />
           </View>
           <Text>BussinessType</Text>
         </View>
         {/* bussiness profile */}
         <View style={{alignItems: 'center'}}>
-          <View
-            style={{
-              backgroundColor: Colors.PRIMARY,
-              borderWidth: 1,
-              borderRadius: 10,
-              borderColor: Colors.TEXT1,
-              height: 40,
-              width: 40,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+          <View style={[
+              formStep === 2 ? {backgroundColor: Colors.PRIMARY} : null,
+              styles.container,
+            ]}>
             <FontAwesome5 name="hand-holding-usd" size={20} />
           </View>
           <Text>BussinessType</Text>
@@ -52,26 +41,51 @@ const BussinessType = () => {
 
         {/* patner */}
         <View style={{alignItems: 'center'}}>
-          <View
-            style={{
-              backgroundColor: Colors.PRIMARY,
-              borderWidth: 1,
-              borderRadius: 10,
-              borderColor: Colors.TEXT1,
-              height: 40,
-              width: 40,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+          <View style={[
+              formStep === 3 ? {backgroundColor: Colors.PRIMARY} : null,
+              styles.container,
+            ]}>
             <Ionicons name="people" size={20} />
           </View>
           <Text>BussinessType</Text>
         </View>
       </View>
+
+      {formStep == 1 && (
+        <View>
+          <Text>Form 1</Text>
+          <CustomButton title={'Next'} onPress={() => setFormStep(2)} />
+        </View>
+      )}
+      {formStep == 2 && (
+        <View>
+          <Text>Form 2</Text>
+          <CustomButton title={'Next'} onPress={() => setFormStep(3)} />
+        </View>
+      )}
+      {formStep == 3 && (
+        <View>
+          <Text>Form 3</Text>
+          <CustomButton
+            title={'Next'}
+            onPress={() => bussinessTypeFormik.handleSubmit()}
+          />
+        </View>
+      )}
     </>
   );
 };
 
 export default BussinessType;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: Colors.TEXT1,
+    height: 40,
+    width: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
